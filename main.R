eb2604cb54911cd53c82961a2f5fe5be1f170834
CGaw <caleb.gaw@gmail.com>
2016-07-31 18:27:06 -0400

    Add files via upload

--- main.R
+++ main.R
@@ -1,6 +1,15 @@
-#reading data from file
-mydata<-read.csv("household_power_consumption.txt",stringsAsFactors=FALSE,sep=";")
-##good<-as.Date(mydata[,1],"%d/%m/%Y")>=as.Date("2007/02/01","%Y/%m/%d") && as.Date(mydata[,1],"%d/%m/%Y")<=as.Date("2007/02/02","%Y/%m/%d")
-good<-as.Date(mydata$Date,"%d/%m/%Y") %in% c(as.Date("01/02/2007","%d/%m/%Y"),as.Date("02/02/2007","%d/%m/%Y"))
-mydata1<-mydata[good,]
-
+main <- function() {
+  ##check if datda file exists, if not download it
+  if(!file.exists("household_power_consumption.txt")) {
+    temp <- tempfile()
+    download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+    file <- unzip(temp)
+    unlink(temp)
+  }
+  ##reading data from file
+  mydata<-read.csv("household_power_consumption.txt",stringsAsFactors=FALSE,sep=";")
+  ##good<-as.Date(mydata[,1],"%d/%m/%Y")>=as.Date("2007/02/01","%Y/%m/%d") && as.Date(mydata[,1],"%d/%m/%Y")<=as.Date("2007/02/02","%Y/%m/%d")
+  good<-as.Date(mydata$Date,"%d/%m/%Y") %in% c(as.Date("01/02/2007","%d/%m/%Y"),as.Date("02/02/2007","%d/%m/%Y"))
+  mydata1<-mydata[good,]
+}
+main()
--- plot1.R
+++ plot1.R
@@ -1,4 +1,9 @@
 #using mydata1 which is filtered 2 day data see main.R for code
-hist(as.numeric(mydata1$Global_active_power),breaks=6,col="red",main="Global Active Power", xlab="Global Active Power(kilowatts")
-dev.copy(png,"plot1.png")
-dev.off()
+plot1 <- function() {
+  par(new=F)
+  par(mfrow=c(1,1))
+  hist(as.numeric(mydata1$Global_active_power),breaks=12,col="red",main="Global Active Power", xlab="Global Active Power(kilowatts)")
+  dev.copy(png, file="plot1.png", width=480, height=480)
+  dev.off()
+}
+plot1()
